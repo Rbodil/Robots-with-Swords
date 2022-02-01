fc35b1b872eab50e9fc19318f886ce378a384bfb
Reagan Bodily <reaganbodily@gmail.com>
2022-01-31 21:56:55 -0700

    Player values and Enemy values js

--- Assets/game.js
+++ Assets/game.js
@@ -1,10 +1,26 @@
 //Player name entry
-window.prompt("Every warrior has a cool name. What do you call yourself?");
+window.alert("Welcome to Robots with Swords, chop a robot to pieces like you're Jeffrey friggin Dahmer!");
 
+// Player Values
 var playerName = window.prompt("Every warrior has a cool name. What do you call yourself?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerCoins = 10;
+
+//Enemy Values
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+console.log(playerName);
 
 // Start Robot Fight
 function fight(){
-    window.alert("Draw swords, time to fight a robot you rapscallion!")
+    window.alert("Draw swords, time to fight a robot " + playerName + "!")
 }
+var fight = function(){
+    window.alert("Meet your Opponent " + enemyName "!")
+};
+
 fight();
