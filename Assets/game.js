e60e9b2d03d903464fa539b870032ca660cf073c
Reagan Bodily <reaganbodily@gmail.com>
2022-01-31 21:29:31 -0700

    bringin branches up to speed

---
+++ Assets/game.js
@@ -0,0 +1,10 @@
+//Player name entry
+window.prompt("Every warrior has a cool name. What do you call yourself?");
+
+var playerName = window.prompt("Every warrior has a cool name. What do you call yourself?");
+
+// Start Robot Fight
+function fight(){
+    window.alert("Draw swords, time to fight a robot you rapscallion!")
+}
+fight();
