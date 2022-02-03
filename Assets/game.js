4409eeef08d1b149236799778a372875c8349556
Reagan Bodily <reaganbodily@gmail.com>
2022-02-02 18:37:26 -0700

    fight functions, first attempt

--- Assets/game.js
+++ Assets/game.js
@@ -17,10 +17,66 @@ console.log(playerName);
 
 // Start Robot Fight
 function fight(){
-    window.alert("Draw swords, time to fight a robot " + playerName + "!")
-}
-var fight = function(){
-    window.alert("Meet your Opponent " + enemyName "!")
+    window.alert("Draw swords, time to fight a robot " + playerName + "!");
+};
+fight();
+
+var enemyintro = function(){
+    window.alert("Meet your Opponent " + enemyName + "!");
+    window.alert("Now fight the motherfucker");
 };
 
-fight();
+enemyintro();
+
+
+function fightenemy(){
+    window.prompt("Will you fight or flee?");
+};
+
+var fightenemy = function(){
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " slashed " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    window.alert( 
+        "You slashed " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+};
+
+fightenemy();
+
+
+
+var enemyfight = function(){
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " hit " + playerName + ", " + playerName+ " has " + playerHealth + " health remaining."
+    );
+    window.alert(
+        enemyName + " slashed you back! Oh shit " + playerName + ", you're bleeding pretty good there! You have " + playerHealth + " health remaining!"
+    );
+    
+    if(enemyHealth === 0){
+        console.log(playerName + " wins!");
+        window.alert(enemyName + "incurred too much damage and commited robot seppoku, " + playerName + " wins!");
+    };
+    else{
+        console.log("attack again.");
+        window.alert(enemyName + " is not impressed, keep fighting " + playerName + "!");
+    };        
+    if(playerHealth === 0){
+        console.log(
+        "Player is dead.");
+        window.alert(){
+        if(playerCoins > 1){
+            "You stagger on your feet due to blood loss. " + enemyName + " chops you in half vertically and takes " + playerCoins + " bloody coins from your pockets while lauging robotically. You die in disgrace."}
+        else{
+            "You stagger on your feet due to blood loss. " + enemyName + " chops you in half vertically and takes " + playerCoins + " bloody coin from your pocket while lauging robotically. You die in disgrace."
+        );
+    };
+};
+
+enemyfight();
+
+
+
